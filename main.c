3036839419f6ab59da830c30652716c780b68343
Unknown <a12607@alunos.ipca.pt>
2018-05-01 10:58:08 +0100

    Ficheiros iniciais do projeto.
    Criação dos ficheiros.

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+Projeto de AED2 - Padaria ....
+Data: 01/05/2018
+Contribuidores: 
+-> Joel Carvalho nº12607
+-> Fábio Gonçalves nº13025
+-> Pedro Falcão nº13047
+*/
+
+void main() {
+	return 0;
+}
